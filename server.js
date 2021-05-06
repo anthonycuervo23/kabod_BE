a903900c37176fc713d96258395e7fe42c14166a
jean cuervo <martoranito@gmail.com>
2021-05-06 17:00:45 +0200

    changed port

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 require("./app/routes/pr.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.MONGODB_URI || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
